5bb1227ec8da8a69a5a82088c47e5f9cf366547b
aqashyy <ashiqmuhammedep@gmail.com>
2024-12-16 16:15:53 +0530

    Created migration and model for users,designation and department

---
+++ database/migrations/2024_12_16_103121_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('dep_id');
+            $table->foreign('dep_id')->references('id')->on('department');
+            $table->unsignedBigInteger('desg_id');
+            $table->foreign('desg_id')->references('id')->on('designation');
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
