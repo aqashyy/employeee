da6967fe20ceeaae52d78bfa07edcffd97910219
aqashyy <ashiqmuhammedep@gmail.com>
2024-12-16 16:54:46 +0530

    Search box and result design created using bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Employee</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
 </head>
 <body>
+<div class="container">
 
+    <div class="row mt-5 d-flex justify-content-center align-items-center">
+        <div class="col-md-6">
+            <div class="form">
+                <label for="search">Search</label>
+                <input type="text" id="searchBox" class="form-control form-input" name="search" placeholder="Search name/designation/department">
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Department</p>
+
+                          </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Department</p>
+
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
 </body>
 </html>
 
